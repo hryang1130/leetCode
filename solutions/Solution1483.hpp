d2fd85487aa1e3b1a972ce7d8ed7c13b75b16e7b
haoran <1946701808@qq.com>
2023-06-12 10:09:15 +0800

    Solution1483

---
+++ solutions/Solution1483.hpp
@@ -0,0 +1,53 @@
+//
+// Created by kevin on 2023/6/12.
+//
+
+#ifndef LEETCODEGITHUB_SOLUTION1483_HPP
+#define LEETCODEGITHUB_SOLUTION1483_HPP
+
+//https://leetcode.cn/problems/kth-ancestor-of-a-tree-node/
+
+#include <vector>
+using namespace std;
+
+//倍增+dp
+class TreeAncestor {
+public:
+    TreeAncestor(int n, vector<int>& parent) {
+        p = vector<vector<int>>(n, vector<int>(18, -1));
+        for (int i = 0; i < n; ++i) {
+            p[i][0] = parent[i];
+        }
+        for (int i = 0; i < n; ++i) {
+            for (int j = 1; j < 18; ++j) {
+                if (p[i][j - 1] == -1) {
+                    continue;
+                }
+                p[i][j] = p[p[i][j - 1]][j - 1];
+            }
+        }
+    }
+
+    int getKthAncestor(int node, int k) {
+        for (int i = 17; ~i; --i) {
+            if (k >> i & 1) {
+                node = p[node][i];
+                if (node == -1) {
+                    break;
+                }
+            }
+        }
+        return node;
+    }
+
+private:
+    vector<vector<int>> p;
+};
+
+/**
+ * Your TreeAncestor object will be instantiated and called as such:
+ * TreeAncestor* obj = new TreeAncestor(n, parent);
+ * int param_1 = obj->getKthAncestor(node,k);
+ */
+
+#endif //LEETCODEGITHUB_SOLUTION1483_H
